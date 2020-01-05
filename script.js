bed29936106ee519e18a31240e54b49cb00cf649
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-06 07:53:47 +0800

    adding script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var x = 10;
-console.log('Hello World!');
-console.log('isi dari variable x adalah ' + x);
+alert('selamat datang..');
+var lagi = true;
+
+while( lagi ) {
+	var nama = prompt('masukkan nama:');
+	alert('halo ' + nama);
+
+	lagi = confirm('coba lagi?');
+}
+
+alert('terima kasih..');
