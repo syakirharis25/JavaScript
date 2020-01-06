3a21b2aaffafddf48035e0c2512e818d3ab3afc3
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-06 10:27:41 +0800

    adding while.js practice file

--- while.js
+++ while.js
@@ -1,5 +1,7 @@
-var nilaiAwal = 1;
-while(nilaiAwal <= 10) {
-	console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.');
-	nilaiAwal++;
+var jmlAngkot = 10;
+var noAngkot = 1;
+
+while(noAngkot <= jmlAngkot) {
+	console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+	noAngkot++;
 }
