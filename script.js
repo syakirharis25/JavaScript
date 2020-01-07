14a5ebae5f57a0538443b1b37cc136eb37fb4ceb
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-08 03:09:42 +0800

    adding script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function jumlahVolumeDuaKubus(a, b) {
-	var volumeA;
-	var volumeB;
-	var total;
+function tambah() {
+	var hasil = 0;
+	for( var i = 0; i < arguments.length; i++ ) {
+		hasil += arguments[i];
+	}
+	return hasil
+}
 
-	volumeA = a * a * a;
-	volumeB = b * b * b;
-
-	total = volumeA + volumeB;
-
-	return total;
- }
-
-alert(jumlahVolumeDuaKubus(8,3));
-alert(jumlahVolumeDuaKubus(10,15));
+var coba = tambah(1,2,3,5);
+console.log(coba);
