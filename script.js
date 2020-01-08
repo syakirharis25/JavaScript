760624c0acecfcb79456a9dad54b781688879bd7
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-09 04:34:20 +0800

    adding script.js

--- script.js
+++ script.js
@@ -46,7 +46,37 @@
  
 // 5. slice
 // slice(awal,akhir)
-var arr = ['Sandhika', 'Galih', 'Nofa', 'Doddy', 'Fitri'];
-var arr2 = arr.slice(1,4);
-console.log(arr.join(' - '));
-console.log(arr2.join(' - '));
+// var arr = ['Sandhika', 'Galih', 'Nofa', 'Doddy', 
+//  'Fitri'];
+// var arr2 = arr.slice(1,4);
+// console.log(arr.join(' - '));
+// console.log(arr2.join(' - '));
+
+// 6. foreach
+var angka = [1,2,3,4,5,6,7,8];
+var nama = ['Sandhika', 'Galih', 'Nofa'];
+// for( var i = 0; i < angka.length; i++ ) {
+// 	console.log(angka[i]);
+// }
+
+// angka.forEach(function(e) {
+// 	console.log(e);
+// });
+// nama.forEach(function(e, i){
+// 	console.log('Mahasiswa ke-' + (i+1) + 
+// 		' adalah : ' + e);
+// })
+
+// 7. map
+// var angka = [1,2,5,3,6,8,4];
+// var angka2 = angka.map(function(e) {
+// 	return e * 2;
+// });
+// console.log(angka2.join(' - '));
+
+// 8. sort
+var angka = [1,2,10,5,20,3,6,8,4];
+angka.sort(function(a,b) {
+	return a-b;
+});
+console.log(angka.join(' - '));
