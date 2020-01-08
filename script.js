ff3985de3fd0dcfde00f25ccb556310ea2f2dbce
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-09 03:47:04 +0800

    adding script.js

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function tampilAngka(n) {
-	if( n === 0 ) return;
-	console.log(n);
-	return tampilAngka(n-1)
-}
+// Manipulasi Array
 
-tampilAngka(10);
+// 1. Menambah isi array
+// var arr = [];
+// arr[0] = "Sandhika";
+// arr[1] = "Galih";
+// arr[2] = "Nofa";
+// arr[6] = "Doddy";
+
+// console.log(arr);
+
+// 2. Menghapus isi array
+// var arr = ["Sandhika", "Galih", "Nofa"];
+// arr[1] = undefined;
+// console.log(arr);
+
+// 3. Menampilkan isi array
+// var arr = ["Sandhika", "Galih", "Nofa"];
+
+// for( var i = 0; i < arr.length; i++ ) {
+// 	console.log('Mahasiswa ke-' + (i+1) + ' : ' + arr[i]);
+// }
+
+
+// Method pada array
+// 1. join
+ var arr = ['Sandhika', 'Galih', 'Nofa'];
+// console.log(arr.join(' - '));
+
+// 2. push & pop
+arr.push('Doddy', 'Fitri'); 
+// arr.pop();
+// arr.pop();
+// console.log(arr.join(' - '));
+
+// 3. unshift & shift
+// arr.unshift('Doddy');
+arr.shift();
+console.log(arr.join(' - '));
