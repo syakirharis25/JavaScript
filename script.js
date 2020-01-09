01c0a245e81791cdf69dd9b286c8e42648c151b6
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-09 13:57:38 +0800

    adding script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var penumpang = ['Sandhika', undefined, 'Doddy'];
+var penumpang = [ ];
 var tambahPenumpang = function(namaPenumpang, penumpang) {
 		// jika angkot kosong
 		if( penumpang.length == 0 ) {
@@ -32,4 +32,20 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
 			}
 		}
 	} 
+}
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+	if( penumpang.length == 0 ) {
+		 console.log('Angkot masih kosong.');
+	} else {
+		for( var i = 0; i < penumpang.length; i++ ) {
+			if( penumpang[i] == namaPenumpang ) {
+				penumpang[i] = undefined;
+			} else if( i == penumpang.length - 1 ) {
+				console.log(namaPenumpang + ' tidak ada di dalam Angkot');
+			}
+		}
+	}
+
+	return penumpang;
 }
