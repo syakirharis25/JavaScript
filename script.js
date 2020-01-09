0f2a29175baeea0965788e5925edf69c4721b1f4
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-10 05:29:52 +0800

    adding script.js

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-// membuat Object
-var mhs = {
-	nama : "Sandhika Galih",
-	umur : 31,
-	ips : [3.00, 2.50, 3.20],
-	alamat : {
-		jalan : "Jl. abc No. 123",
-		kota : "Bandung",
-		provinsi : "Jawa Barat"
-	}
-};
+// Membuat Object
+// Object Literal
+var mhs1 = {
+	nama : 'Sandhika Galih',
+	nrp : '043040023',
+	email : 'sandhikagalih@unpas.ac.id',
+	jurusan : 'Teknik Informatika'
+}
+
+var mhs2 = {
+	nama : 'Doddy',
+	nrp : '033040007',
+	email : 'doddy@gmail.com',
+	jurusan : 'Teknik Industri'
+}
+
+// Function Declaration
+function buatObjectMahasiswa(nama, nrp, email, jurusan) {
+	var mhs = {};
+	mhs.nama = nama;
+	mhs.nrp = nrp;
+	mhs.email = email;
+	mhs.jurusan = jurusan;
+	return mhs; 
+}
+
+var mhs3 = buatObjectMahasiswa('Nofariza', '023040123', 
+	'nofa@yahoo.com', 'Teknik Pangan')
+
+// Constructor
+function Mahasiswa(nama, nrp, email, jurusan) {
+	// var this = {};
+	this.nama = nama;
+	this.nrp = nrp;
+	this.email = email;
+	this.jurusan = jurusan;
+	// return this;
+}
+
+var mhs4 = new Mahasiswa('Erik', '013040321', 
+	'erik@cloud.com', 'Teknik Mesin');
