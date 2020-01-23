3f475df0827a6e84e68e855c01674869f2f4e0c6
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-23 16:09:43 +0800

    adding modified code folder

--- code/script.js
+++ code/script.js
@@ -1,21 +1,24 @@
-// DOM Selection
-// getElementById() -> element
-const judul = document.getElementById('judul');
-judul.style.color = 'red';
-judul.style.backgroundColor = '#ccc';
-judul.innerHTML = 'Sandhika Galih';
+// // DOM Selection
+// // getElementById() -> element
+// // document.getElementsByTagName() -> HTMLCollections
+// // document.getElementsByClassName()
+ 
+// // document.querySelector() -> element
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+// p4.style.fontSize = '30px';
 
-// document.getElementsByTagName() -> HTMLCollections
-const p = document.getElementsByTagName('p');
+// const li2 = document.querySelector('section#b ul li:nth-child(2');
+// li2.style.backgroundColor = 'orange';
 
-for( let i = 0; i < p.length; i++ ) {
-	p[i].style.backgroundColor = 'lightblue';
-}
+// // document.querySelectorAll()
+// const p = document.querySelectorAll('p');
+// for (let i=0; i<p.length; i++) {
+// 	p[i].style.backgroundColor = 'lightblue';
+// }
 
-const h1 = document.getElementsByTagName('h1')[0];
-h1.style.fontSize = '50px';
 
-// document.getElementsByClassName()
-// -> HTMLCollection
-const p1 = document.getElementsByClassName('p1')[0];
-p1.innerHTML = 'Ini diubah dari JavaScript';
+
+const sectionB = document.querySelector('section#b');
+const p4 = sectionB.getElementsByTagName('p')[0];
+p4.style.backgroundColor = 'orange';
