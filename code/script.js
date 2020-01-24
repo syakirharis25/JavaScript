20c02b0c7e0c93dfa7212c765197b8269f8b7db5
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-24 22:12:24 +0800

    adding modified code/index.html and code/script.js

--- code/script.js
+++ code/script.js
@@ -1,40 +1,35 @@
-// DOM Manipulation
-// buat elemen baru
-const pBaru = document.createElement('p');
-const teksPBaru = document.createTextNode('Paragraf Baru');
-// simpan tulisan ke dalam paragraf
-pBaru.appendChild(teksPBaru);
+// const p3 = document.querySelector('.p3');
 
-// simpan pBaru di akhir Section A
-const sectionA = document.getElementById('a');
-sectionA.appendChild(pBaru);
+// function ubahWarnaP2() {
+// 	p2.style.backgroundColor = 'lightblue';
+// }
 
+// function ubahWarnaP3() {
+// 	p3.style.backgroundColor = 'lightblue';
+// }
 
-const liBaru = document.createElement('li');
-const teksLiBaru = document.createTextNode('Item Baru');
-liBaru.appendChild(teksLiBaru);
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahWarnaP2;
 
-const ul = document.querySelector('section#b ul');
-const li2 = ul.querySelector('li:nth-child(2)');
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function() {
+// 	const ul = document.querySelector('section#b ul');
+// 	const liBaru = document.createElement('li');
+// 	const teksLiBaru = document.createTextNode('item baru');
+// 	liBaru.appendChild(teksLiBaru);
+// 	ul.appendChild(liBaru);
+// });
 
-ul.insertBefore(liBaru, li2);
-
-
-const link = document.getElementsByTagName('a')[0];
-
-sectionA.removeChild(link);
-
-
-const sectionB = document.getElementById('b');
-const p4 = sectionB.querySelector('p');
-
-const h2Baru = document.createElement('h2');
-const teksH2Baru = document.createTextNode('Judul baru!');
-
-h2Baru.appendChild(teksH2Baru);
-
-sectionB.replaceChild(h2Baru, p4);
-
-pBaru.style.backgroundColor = 'lightgreen';
-liBaru.style.backgroundColor = 'lightgreen';
-h2Baru.style.backgroundColor = 'lightgreen';
+const p3 = document.querySelector('.p3');
+// p3.onclick = function() {
+// 	p3.style.backgroundColor = 'lightblue';
+// }
+// p3.onclick = function() {
+// 	p3.style.color = 'red';
+// }
+p3.addEventListener('mouseenter', function() {
+	p3.style.backgroundColor = 'lightblue';
+});
+p3.addEventListener('mouseleave', function() {
+	p3.style.backgroundColor = 'lightgreen';
+});
