bfff6644386f58f225fc79471189e96f07f37ab9
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-01-28 00:49:51 +0800

    adding modified code/script.js file

--- code/script.js
+++ code/script.js
@@ -1,16 +1,4 @@
-// Template Literals / Template String
-const nama = 'Sandhika';
-const umur = 33;
-// console.log(`Halo, nama saya ${nama}, dan saya ${umur} tahun.`);
-// console.log('Halo, nama saya ' + nama + ', dan saya ' + umur + ' tahun.');
-
-// Embedded Expression
-// console.log(`${1 + 1}`)
-// console.log(`${alert('halo')}`);
-// const x = 11;
-// console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`);
-
-// HTML Fragments
+// 1. HTML Fragments
 // const mhs = {
 //     nama: 'Sandhika Galih',
 //     umur: 33,
@@ -18,9 +6,79 @@ const umur = 33;
 //     email: 'sandhikagalih@unpas.ac.id'
 // };
 
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nrp">${mhs.nrp}</span>
+// </div>`
+
+// console.log(el);
+
+// 2. Looping
+// const mhs = [
+//     {
+//         nama: 'Sandhika Galih',
+//         email: 'sandhikagalih@unpas.ac.id'
+//     },
+//     {
+//         nama: 'Doddy Ferdiansyah',
+//         email: 'sandhikagalih@unpas.ac.id'
+//     },
+//     {
+//         nama: 'Erik',
+//         email: 'sandhikagalih@unpas.ac.id'
+//     }
+// ];
+
+// const el = `<div class="mhs">
+//     ${mhs.map(m => `<ul>
+//     <li>${m.nama}</li>
+//     <li>${m.email}</li>
+//     </ul>`).join('')}
+// </div>`;
+
+// 3. Conditionals
+// ternary
+// const lagu = {
+//     judul: 'Kau Adalah',
+//     penyanyi: 'Isyana Sarasvati',
+//     feat: 'Rayi Putra'
+// }
+
+// const el = `<div class="lagu">
+//     <ul>
+//         <li>${lagu.penyanyi}</li>
+//         <li>${lagu.judul} ${lagu.feat ? `(feat. ${lagu.feat})` : ''}</li>
+//     </ul>
+// </div>`;
+
+
+// 4. Nested
+// HTML Fragments bersarang
+
+const mhs = {
+    nama: 'Sandhika Galih',
+    semester: 5,
+    mataKuliah: [
+        'Rekayasa Web', 
+        'Analisis dan Perancangan Sistem Informasi',
+        'Pemrograman Sistem Interaktif',
+        'Perancangan Sistem Berorientasi Object'
+    ]
+};
+
+function cetakmataKuliah(mataKuliah) {
+    return `
+        <ol>
+           ${mataKuliah.map(mk => `<li>${mk}</li>`).join('')}
+        </ol>
+    `;
+}
+
 const el = `<div class="mhs">
     <h2>${mhs.nama}</h2>
-    <span class="nrp">${mhs.nrp}</span>
+    <span class="semester">Semester : ${mhs.semester}</span>
+    <h4>Mata Kuliah :</h4>
+    ${cetakmataKuliah(mhs.mataKuliah)}
 </div>`;
 
-console.log(el);
+document.body.innerHTML = el;
